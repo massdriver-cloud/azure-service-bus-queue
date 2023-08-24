0845b5556d8d7010d76a1ebf2f58aeca57adac8f
mclacore <michael@massdriver.cloud>
2023-08-24 12:00:20 -0600

    Fix format

--- src/main.tf
+++ src/main.tf
@@ -5,14 +5,14 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_servicebus_namespace" "main" {
-  name                          = var.md_metadata.name_prefix
-  location                      = azurerm_resource_group.main.location
-  resource_group_name           = azurerm_resource_group.main.name
-  sku                           = var.service.sku
-  capacity                      = var.service.sku == "Premium" ? var.service.capacity : 0
-  minimum_tls_version           = "1.2"
-  zone_redundant                = var.service.sku == "Premium" ? var.service.zone_redundant : false
-  tags                          = var.md_metadata.default_tags
+  name                = var.md_metadata.name_prefix
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = var.service.sku
+  capacity            = var.service.sku == "Premium" ? var.service.capacity : 0
+  minimum_tls_version = "1.2"
+  zone_redundant      = var.service.sku == "Premium" ? var.service.zone_redundant : false
+  tags                = var.md_metadata.default_tags
 
   identity {
     type = "SystemAssigned"
